8700970070740b471c13f7275ff9895b360b229c
tipoud <douay.n@live.fr>
2018-05-24 11:48:28 +0200

    setup home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,37 @@ export class HomeComponent implements OnInit {
 
   public title = 'Home';
   public rightIcon = 'fas fa-search';
-  constructor() { }
-
+  public deviceUrl = 'https://whatwebcando.today/';
+  public sanitizer: DomSanitizer;
+  constructor(sanitizer: DomSanitizer) {
+    this.sanitizer = sanitizer;
+  }
   ngOnInit() {
   }
 
+  public displayWebFrame(device: DeviceURL) {
+    switch (device) {
+      case DeviceURL.android: {
+        this.deviceUrl = 'https://whatwebcando.today/';
+        break;
+      }
+      case DeviceURL.ios: {
+        this.deviceUrl = 'https://www.igen.fr/ios/2018/04/testez-les-progressive-web-apps-maintenant-quios-113-les-prend-en-charge-103525'
+        break;
+      }
+      case DeviceURL.google: {
+        this.deviceUrl = 'https://www.google.fr/';
+        break;
+      }
+    }
+  }
+
+
+
+}
+
+enum DeviceURL {
+  android,
+  ios,
+  google
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from './home.component';
 import {LayoutModule} from '../layout/layout.module';
+import {MatButtonToggleModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    LayoutModule
+    LayoutModule,
+    MatButtonToggleModule
   ],
   declarations: [HomeComponent]
 })
